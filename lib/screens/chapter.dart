0ca16fdd2fecaf488602dbd34e0f2f8157d8472c
JosephMcCoy315 <59934517+JosephMcCoy315@users.noreply.github.com>
2020-04-20 04:33:29 -0400

    Update chapter.dart
    structure is there just need the api to see how many chapter button will be displayed and then a on pressed function take to a screen with all the text

--- lib/screens/chapter.dart
+++ lib/screens/chapter.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:buildabook/widgets/widget.dart';
+import 'package:buildabook/widgets/widget.dart';
+import 'books.dart';
+import 'winners.dart';
+import 'contenders.dart';
 
 
-class ChapterPage extends StatefulWidget {
+class ChapterPage extends StatelessWidget {
   static String tag = 'chapter-page';
-  @override
-  _ChapterPageState createState() => new _ChapterPageState();
-}
 
-class _ChapterPageState extends State<ChapterPage> {
+  //final Books book;
+
+  //ChapterPage(this.book);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: BaseAppBar(title: Text('Chapter'), appBar: AppBar()),
       drawer: BaseMenu(),
-    );
+        body:Column(
+          children: <Widget>[
+            for (int i =1; i < 4;i++ ) // intiial loop need to add the api to get number of chapters so loop can be right
+              new SizedBox(
+                width: 420,
+                height: 100,
+                child: new RaisedButton(
+                  child: Text('Chapter '+ i.toString()),
+                   onPressed: (){// jus need to grab the text and prirt it. 
+                     // i++;
+                  } ,
+                ),
+              )
+          ],
+        ),
+
+      );
   }
+
 }
