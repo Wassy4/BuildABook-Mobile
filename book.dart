e73fb98d1e8164db994e877cc483d2b818f5da8d
JosephMcCoy315 <59934517+JosephMcCoy315@users.noreply.github.com>
2020-04-20 04:28:16 -0400

    Add files via upload
    book added a read chapter function to get change the page to chapters

---
+++ book.dart
@@ -0,0 +1,86 @@
+import 'package:buildabook/screens/chapter.dart';
+import 'package:flutter/material.dart';
+import 'package:buildabook/widgets/widget.dart';
+import 'books.dart';
+import 'winners.dart';
+import 'contenders.dart';
+
+
+class BookPage extends StatelessWidget {
+  static String tag = 'book-page';
+
+  Books book;
+
+  BookPage(this.book);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: BaseAppBar(title: Text(book.title), appBar: AppBar()),
+      body: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(15.0),
+                  child: Image.network(book.image, height: 200, width: 200),
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("Author(s):", style: TextStyle(fontWeight: FontWeight.bold)),
+                      for (var author in book.authorArray)
+                        Text(author),
+                      Padding(
+                          padding: EdgeInsets.all(5.0)
+                      ),
+                      Text("Date Created:", style: TextStyle(fontWeight: FontWeight.bold)),
+                      Text(book.dateCreated),
+                      Padding(
+                          padding: EdgeInsets.all(5.0)
+                      ),
+                      Text("Number of Chapters:", style: TextStyle(fontWeight: FontWeight.bold)),
+                      Text(book.numberOfChapters.toString()),
+                      Padding(
+                          padding: EdgeInsets.all(5.0)
+                      ),
+                      Text("Views:", style: TextStyle(fontWeight: FontWeight.bold)),
+                      Text(book.views.toString()),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  width: 335,
+                  margin: EdgeInsets.only(left: 15.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("Writing Prompt:", style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+                      Padding(
+                        padding: EdgeInsets.all(2.0),
+                      ),
+                      Text(book.writingPrompt),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            RaisedButton(
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ChapterPage()));
+              } ,
+              child: Text('Read Chapters'),
+            ),
+          ]
+
+        ),
+      );
+  }
+}
